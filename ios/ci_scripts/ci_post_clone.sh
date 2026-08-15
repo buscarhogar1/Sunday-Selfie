9e651fc2b385ddda4c47dbfbcee8917052f5a4fb
Lucrezia Lafratta <lomarcer@gmail.com>
2026-08-15 12:13:12 +0200

    Usar módulos reales de Pods en Xcode Cloud

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -28,38 +28,4 @@ echo "Installing iOS pods..."
 cd "$REPO_ROOT/ios"
 pod install --repo-update
 
-add_flutter_plugin_header() {
-  module_name="$1"
-  header_name="$2"
-  class_name="$3"
-  header_path="$REPO_ROOT/ios/Pods/Headers/Public/$module_name/$header_name"
-
-  if [ -f "$header_path" ]; then
-    echo "Keeping existing public header for $module_name/$header_name"
-    return 0
-  fi
-
-  mkdir -p "$(dirname "$header_path")"
-  cat > "$header_path" <<EOF
-#import <Flutter/Flutter.h>
-
-@interface $class_name : NSObject <FlutterPlugin>
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar;
-@end
-EOF
-}
-
-echo "Adding compatibility headers for Flutter plugins..."
-add_flutter_plugin_header "camera_avfoundation" "CameraPlugin.h" "CameraPlugin"
-add_flutter_plugin_header "cloud_firestore" "FLTFirebaseFirestorePlugin.h" "FLTFirebaseFirestorePlugin"
-add_flutter_plugin_header "cloud_functions" "FirebaseFunctionsPlugin.h" "FirebaseFunctionsPlugin"
-add_flutter_plugin_header "firebase_app_check" "FLTFirebaseAppCheckPlugin.h" "FLTFirebaseAppCheckPlugin"
-add_flutter_plugin_header "firebase_auth" "FLTFirebaseAuthPlugin.h" "FLTFirebaseAuthPlugin"
-add_flutter_plugin_header "firebase_core" "FLTFirebaseCorePlugin.h" "FLTFirebaseCorePlugin"
-add_flutter_plugin_header "firebase_messaging" "FLTFirebaseMessagingPlugin.h" "FLTFirebaseMessagingPlugin"
-add_flutter_plugin_header "firebase_storage" "FLTFirebaseStoragePlugin.h" "FLTFirebaseStoragePlugin"
-add_flutter_plugin_header "google_mobile_ads" "FLTGoogleMobileAdsPlugin.h" "FLTGoogleMobileAdsPlugin"
-add_flutter_plugin_header "google_sign_in_ios" "FLTGoogleSignInPlugin.h" "FLTGoogleSignInPlugin"
-add_flutter_plugin_header "image_picker_ios" "FLTImagePickerPlugin.h" "FLTImagePickerPlugin"
-add_flutter_plugin_header "share_plus" "FPPSharePlusPlugin.h" "FPPSharePlusPlugin"
-add_flutter_plugin_header "webview_flutter_wkwebview" "WebViewFlutterPlugin.h" "WebViewFlutterPlugin"
+echo "CocoaPods installation completed. Xcode will use the real plugin modules."
